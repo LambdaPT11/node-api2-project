a9a54721d79c16e216b27e47a4e013bfdea977ae
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-03-17 20:00:33 -0400

    finished project crud endpoints.

--- server/router/Posts-Router.js
+++ server/router/Posts-Router.js
@@ -12,6 +12,48 @@ const router = express.Router();
 // will call that import Posts
 const Posts = require('../../data/db.js');
 
+// .post("/api/posts")
+router.post('/', (req, res) => {
+  const postInfo = req.body;
+  const { title, contents } = req.body;
+
+  if (title === '' || null) {
+    res.status(400).json({ msg: 'Please provide title and contents for the post.' });
+  } else if (contents === '' || null) {
+    res.status(400).json({ msg: 'Please provide title and contents for the post.' });
+  } else {
+    Posts
+      .insert(postInfo)
+      .then(() => {
+        res.status(201).json(postInfo);
+      });
+  }
+});
+
+// .post("/api/posts/:id/comments")
+// error - not working
+router.post('/:id/comments', (req, res) => {
+  // eslint-disable-next-line camelcase
+  const { post_id, text } = req.body;
+
+  if (text === '') {
+    res.status(400).json({ msg: 'Please provide text for the comment.' });
+  }
+
+  Posts.insertComment(req.body)
+    .then((comment) => {
+      // eslint-disable-next-line camelcase
+      if (post_id) {
+        res.status(201).json(comment);
+      } else {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ err: 'There was an error while saving the comment to the database' });
+    });
+});
+
 // When the client makes a GET request to /api/posts
 // If there's an error in retrieving the posts from the database:
 // cancel the request.
@@ -31,5 +73,90 @@ router.get('/', (req, res) => {
 // This does return an array of post objects
 // error checking works if fail in db.js
 
+//  .get("/api/posts/:id")
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Posts.findById(id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ err: 'The post information could not be retrieved.' });
+    });
+});
+
+// get(api/posts/)
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+  Posts.findPostComments(id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      }
+    });
+});
+
+// Delete - .delete('/api/posts/)
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Posts.findById(id)
+    .then((post) => {
+      if (post) {
+        Posts.remove(id)
+          .then((deleted) => {
+            if (deleted) {
+              console.log(deleted);
+              res.status(204).json(post);
+            } else {
+              res.status(500).json({
+                error: 'The post could not be removed',
+              });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: 'The post could not be removed',
+            });
+          });
+      } else {
+        res.status(404).json({
+          message: 'The post with the specified ID does not exist.',
+        });
+      }
+    });
+});
+
+// PUT - .put(/api/posts)
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+  const { id, title, contents } = changes;
+
+  if (!(title && contents)) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.',
+    });
+  } else {
+    Posts.update(id, changes)
+      .then((post) => {
+        // eslint-disable-next-line no-unused-expressions
+        post
+          ? res.status(200).json(post)
+          : res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: 'The post information could not be modified.',
+        });
+      });
+  }
+});
 
 module.exports = router;
